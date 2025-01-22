b1b374cc498bd16c32da16b91dcdf2dc7d890636
alenash1988 <Aleonash1988@mail.ru>
2025-01-22 11:55:17 +0900

    Add files via upload

--- modul_16_3.py
+++ modul_16_3.py
@@ -12,24 +12,29 @@ async def get_users() -> dict:
 
 
 @app.post("user/{username}/{age}")
-async def registered_user(username: str, age: int) -> str:
+async def registered_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                        example='UrbanUser')],
+                          age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> str:
     current_id = str(int(max(users, key=int)) + 1)
     users[current_id] = f"Имя: {username}, возраст: {age}"
     return f"User {current_id} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, username: str, age: Annotated[int, Path(gt=0)]) -> str:
-    if user_id not in users:
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                    example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=24)]) -> str:
+    if str(user_id) not in users:
         raise HTTPException(status_code=404, detail=f"User {user_id} not found")
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} is updated"
+    users[str(user_id)] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} has been updated"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str) -> str:
-    if user_id not in users:
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)]) -> str:
+    if str(user_id) not in users:
         raise HTTPException(status_code=404, detail=f"User {user_id} not found")
-    users.pop(user_id)
-    return f"The user {user_id} is deleted"
+    users.pop(str(user_id))
+    return f"User {user_id} has been deleted"
 
